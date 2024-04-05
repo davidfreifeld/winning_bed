b7390b0a60ab7e8df256eeb51565b624b09796ea
David Freifeld <david.freifeld@gmail.com>
2024-04-05 14:33:27 -0600

    requirements file and left right cols

--- winning_bed_streamlit.py
+++ winning_bed_streamlit.py
@@ -13,16 +13,19 @@ def on_run_click():
     results_dict = this_winning_bed.calc_prices_brams_kilgour()
     st.session_state.results_df = this_winning_bed.get_results_df(results_dict)
     
-
 title = 'Winning Bed'
 st.set_page_config(page_title=title)
 st.title(title)
 
-uploaded_bids_file = st.file_uploader(label='Bids CSV File', type='csv')
-if uploaded_bids_file is not None:
-    bids_df = pd.read_csv(uploaded_bids_file, index_col=0)
+left_col, right_col = st.columns(2)
 
-house_cost = st.number_input('Total cost of the rental', step=1, value=1900)
+with left_col:
+    uploaded_bids_file = st.file_uploader(label='Bids CSV File', type='csv')
+    if uploaded_bids_file is not None:
+        bids_df = pd.read_csv(uploaded_bids_file, index_col=0)
+
+with right_col:
+    house_cost = st.number_input('Total cost of the rental', step=1, value=1900)
 
 st.button('Run', on_click=on_run_click, type="primary")
 
